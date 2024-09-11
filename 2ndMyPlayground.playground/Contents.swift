a416b7a206df3db79c9a2ebbd41fb530306a7fa7
Aero <aero@gmail.com>
2024-09-11 15:23:30 +0500

    Initial Commit

---
+++ 2ndMyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+let const1 = 24
+let const2 = 25
+print(const1 + const2)
+
+var peremen1 = "Monte"
+peremen1 = "Monte Kristo"
+let peremen2 = " = "
+
+
+var c = peremen1 + peremen2
+typealias myType = Int
+
+var oneHundred: myType = 100
+
+typealias doubleType = Double
+
+let double: doubleType = 67.5
+
+let c3 = oneHundred + Int(double) + 23
+
+
